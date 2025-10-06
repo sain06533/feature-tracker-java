704655983a9659435f8fda8855c3c75a1af6b283
SaiKodali-stack <saikodali@deloitte.com>
2025-10-06 10:33:43 +0530

    Implemented addFeature method

--- src/com/example/FeatureTracker.java
+++ src/com/example/FeatureTracker.java
@@ -1,7 +1,18 @@
 package com.example;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class FeatureTracker {
     public static void main(String[] args) {
-        System.out.println("Feature Tracker App");
+        addFeature("Login functionality");
+    }
+
+    public static void addFeature(String feature) {
+        try (FileWriter writer = new FileWriter("features.txt", true)) {
+            writer.write(feature + "\n");
+            System.out.println("Feature added: " + feature);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
