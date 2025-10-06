6696abe38340f9b93f4c7a7cbfc565bf7f85bdb1
sain06533 <sain06533@gmail.com>
2025-10-06 10:47:10 +0530

    added remove feature method

--- src/com/example/FeatureTracker.java
+++ src/com/example/FeatureTracker.java
@@ -4,4 +4,7 @@ public class FeatureTracker {
     public static void main(String[] args) {
         System.out.println("Feature Tracker App");
     }
+    public static void removeFeature(String feature) {
+    System.out.println("Feature removed: " + feature);
+}
 }
